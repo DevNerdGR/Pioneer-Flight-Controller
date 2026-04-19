ecbade0c01c6c126352f94148b363f6da26d0545
eagle <eagle@gr.t>
2026-04-19 19:47:10 +0800

    Store

--- PioneerEFC.c
+++ PioneerEFC.c
@@ -1,25 +1,33 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
+#include "hardware/uart.h"
 
+#define BAUD_RATE 420000
+#define UART_ID uart1
+#define UART_TX_PIN 4
+#define UART_RX_PIN 5
 
+void init_elrs_rx() {
+    uart_init(UART_ID, BAUD_RATE);
+    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+}
 
-
-int main()
-{
+void init() {
     stdio_init_all();
+    init_elrs_rx();
+}
 
-    // Initialise the Wi-Fi chip
-    if (cyw43_arch_init()) {
-        printf("Wi-Fi init failed\n");
-        return -1;
-    }
 
-    // Example to turn on the Pico W LED
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+int main() {
+    init();   
 
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        if (uart_is_readable(UART_ID)) {
+            uint8_t data = uart_getc(UART_ID);
+            printf("%02x", data);
+        }
     }
+
 }
